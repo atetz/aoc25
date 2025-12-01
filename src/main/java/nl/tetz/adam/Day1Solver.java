a102ea65f86d2b1a9e2d4f39a69d4d4f4af606a9
Adam Tetz <mail@adamtetz.nl>
2025-12-01 17:25:39 +0100

    Project setup

---
+++ src/main/java/nl/tetz/adam/Day1Solver.java
@@ -0,0 +1,18 @@
+package nl.tetz.adam;
+
+
+public class Day1Solver {
+    void main() {
+        IO.println(solvePart1());
+        IO.println(solvePart1());
+    }
+
+
+    public int solvePart1() {
+        return 1;
+    }
+
+    public int solvePart2() {
+        return 2;
+    }
+}
--- src/main/java/nl/tetz/adam/utils/FileHelper.java
+++ src/main/java/nl/tetz/adam/utils/FileHelper.java
@@ -1,4 +1,37 @@
 package nl.tetz.adam.utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class FileHelper {
+    private static final Path INPUT_PATH = Path.of("src/main/resources/puzzleInput");
+
+    public static List<String> readLines(String fileName) {
+        try {
+            return Files.readAllLines(INPUT_PATH.resolve(fileName));
+        } catch (IOException e) {
+                throw new RuntimeException("Failed to read file: " + fileName, e);
+            }
+        }
+
+
+    public static List<int[]> readLinesAsListOfIntArray(String fileName, String delimiter) {
+        return readLines(fileName).stream()
+                .map(line -> Arrays.stream(line.split(delimiter))
+                        .mapToInt(Integer::parseInt)
+                        .toArray())
+                .toList();
+
+    }
+
+    public static String[][] readLinesAs2DArray(String fileName) {
+        return readLines(fileName).stream().map(s -> s.split(""))
+                .toArray(String[][]::new);
+    }
 }
+
+
