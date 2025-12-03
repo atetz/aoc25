0f107d9d122d05a68c02b1641e86eb3e0226cc69
Adam Tetz <mail@adamtetz.nl>
2025-12-03 10:34:01 +0100

    clean up dirty day2.

--- src/main/java/nl/tetz/adam/Day2Solver.java
+++ src/main/java/nl/tetz/adam/Day2Solver.java
@@ -8,54 +8,50 @@ import java.util.List;
 
 public class Day2Solver {
 
-    public BigInteger solvePart1(List<String> ranges) {
-
-        BigInteger total = BigInteger.ZERO;
-
-        for (String range : ranges) {
-            BigInteger start = new BigInteger(range.split("-")[0]);
-            BigInteger end = new BigInteger(range.split("-")[1]);
-
-            for (BigInteger i = start; i.compareTo(end) <= 0; i = i.add(BigInteger.ONE)) {
-                String productId = String.valueOf(i);
-                int productIdLength = productId.length();
-                if (productIdLength % 2 == 0) {
-                    String firstPart = productId.substring(0, productIdLength / 2);
-                    String secondPart = productId.substring((productIdLength / 2), productIdLength);
-                    if (firstPart.equals(secondPart)) {
-                        total = total.add(i);
-                    }
-
-                }
-            }
+    public boolean validateP1ProductId(long productId) {
+        String productIdString = String.valueOf(productId);
+        int productIdLength = productIdString.length();
+        if (productIdLength % 2 == 0) {
+            String firstPart = productIdString.substring(0, productIdLength / 2);
+            String secondPart = productIdString.substring((productIdLength / 2), productIdLength);
+            return firstPart.equals(secondPart);
         }
-        return total;
+        return false;
     }
 
-    public BigInteger solvePart2(List<String> ranges) {
+    public boolean validateP2ProductId(long productId) {
+        String productIdString = String.valueOf(productId);
+        int productIdLength = productIdString.length();
 
-        // In hindsight this is a brute-force approach to string periodicity detection
+        // if pos has passed length /2 then repetition of string wil be too big for length of product id.
 
+        for (int pos = 1; pos <= productIdLength / 2; pos++) {
+            String pattern = productIdString.substring(0, pos);
+            String repeated = pattern.repeat((productIdLength / pos));
+
+            if (repeated.startsWith(productIdString)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public BigInteger solver(List<String> ranges, boolean part2) {
 
         BigInteger total = BigInteger.ZERO;
 
         for (String range : ranges) {
-            BigInteger start = new BigInteger(range.split("-")[0]);
-            BigInteger end = new BigInteger(range.split("-")[1]);
+            long start = Long.parseLong(range.split("-")[0]);
+            long end = Long.parseLong(range.split("-")[1]);
 
-            for (BigInteger i = start; i.compareTo(end) <= 0; i = i.add(BigInteger.ONE)) {
-                String productId = String.valueOf(i);
-                int productIdLength = productId.length();
-
-                // if pos has passed length /2 then repetition of string wil be too big for length of product id.
-
-                for (int pos = 1; pos <= productIdLength / 2; pos++) {
-                    String pattern = productId.substring(0, pos);
-                    String repeated = pattern.repeat((productIdLength / pos));
-
-                    if (repeated.startsWith(productId)) {
-                        total = total.add(i);
-                        break;
+            for (long i = start; i <= end; i++) {
+                if (!part2) {
+                    if (validateP1ProductId(i)) {
+                        total = total.add(BigInteger.valueOf(i));
+                    }
+                } else {
+                    if (validateP2ProductId(i)) {
+                        total = total.add(BigInteger.valueOf(i));
                     }
                 }
             }
@@ -69,10 +65,9 @@ public class Day2Solver {
         List<String> lines = fileHelper.readLines("day2.txt");
         List<String> ranges = Arrays.stream(lines.getFirst().split(",")).toList();
 
-        System.out.printf("Part one: %d \n", solvePart1(ranges));
-
-        System.out.printf("Part two: %d", solvePart2(ranges));
-
+        System.out.printf("Part one: %d \n", solver(ranges, false));
+        System.out.printf("Part two: %d \n", solver(ranges, true));
+        
     }
 
 }
